09a8484a80111b1074a65e8c257fd84f5689f57f
MaksimNorvatkin <norvatkin_maksim@mail.ru>
2025-04-17 00:50:50 +0500

    начал делать задания3

--- practica/Task02.java
+++ practica/Task02.java
@@ -1,21 +1,30 @@
 package example.practica;
 
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import example.model.Student;
 import example.model.StudentData;
 
 /**
- * Практика
+ * Практика3
+ * // FIXME с помошью stream APi для каждой группы выведи число студентов
+ * 		// младше 33 лет
+ * 		// подсказка используй групировку или flatMap
  */
 public class Task02 {
-	
+
 	public static void main(String[] args) {
-		
-		Stream<Student> stream = StudentData.getPersons();
-		stream.forEach(System.out::println);
-		// FIXME с помошью stream APi для каждой группы выведи число студентов
-		// младше 33 лет
-		// подсказка используй групировку или flatMap
+		Map<String, Long> countByGroup = StudentData.getPersons()
+				.filter(student -> student.getAge() < 33)
+				.collect(Collectors.groupingBy(
+						student -> student.getGroup().getName(),
+						Collectors.counting()
+				));
+
+		countByGroup.forEach((groupName, count) ->
+				System.out.println("Группа " + groupName + ": " + count + " студентов младше 33 лет")
+		);
 	}
 }
--- practica/Task03.java
+++ practica/Task03.java
@@ -1,19 +1,25 @@
 package example.practica;
 
+import java.util.OptionalDouble;
 import java.util.stream.Stream;
 
+import example.model.SexType;
 import example.model.Student;
 import example.model.StudentData;
 
 /**
  * Практика 3
+ * // FIXME с помошью stream APi найди средний возраст всех мужчин группы java 2
  */
 public class Task03 {
 	
 	public static void main(String[] args) {
-		
-		Stream<Student> stream = StudentData.getPersons();
-		stream.forEach(System.out::println);
-		// FIXME с помошью stream APi найди средний возраст всех мужчин группы java 2
+
+		OptionalDouble stream = StudentData.getPersons()
+		.filter(student -> "java2".equals(student.getGroup().getName()))
+				.filter(s->s.getSexType() == SexType.MALE)
+				.mapToInt(Student::getAge)
+				.average();
+		System.out.println("средний возраст всех мужчин группы java 2"+ stream.getAsDouble());
 	}
 }
